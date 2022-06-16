080af559db1b46600c2a8c9f78fc7d056298f317
Alondra Valenzuela <avalenzuela736@gmail.com>
2022-06-16 17:12:06 -0400

    clases java actualizadas AV

---
+++ ProyectoG6/src/main/java/com/generationg6/models/Roles.java
@@ -0,0 +1,10 @@
+package com.generationg6.models;
+/*
+CREATE TABLE rolesUsuarios(
+id_rol_usuario int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nombre_rol varchar(30),
+descripcion_rol varchar(100)
+); */
+public class Roles {
+	
+}
