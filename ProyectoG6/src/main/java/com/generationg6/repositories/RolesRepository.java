c30b5c7fc073613cfee237ec6fe9bdf5980d9055
UnpaidMC <michael.roldan@sansano.usm.cl>
2022-07-11 20:48:09 -0400

    Agregado registro y login/limpie unas relaciones que no eran necesarias

--- ProyectoG6/src/main/java/com/generationg6/repositories/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.generationg6.repositories;
-
-import com.generationg6.models.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolesRepository extends JpaRepository<Roles, Long> {
-}
