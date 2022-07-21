3ad2b3ffa5a83e439916ec47d1f7f742263f9f5c
Alondra Valenzuela <avalenzuela736@gmail.com>
2022-07-21 15:49:30 -0400

    agregado imagenes semillita etc

--- FRONTEND/semillita/src/components/FormularioRegistroComponent.js
+++ FRONTEND/semillita/src/components/FormularioRegistroComponent.js
@@ -99,10 +99,6 @@ return(
                 onChange={handleInputChange}
                 />
                 
-                {/*INYECTADO LINK AL BOTON*/}
-                <a href="asignaturas.html">
-                  <input id="btn1" type="submit" defaultValue="Registrarse" />
-                </a>
               </div>
               <div className="columna2 col text-dark">
                 <div className="btn-selector">
@@ -114,6 +110,12 @@ return(
                   </div>
                 </div>
               </div>
+            <div className="asdf">
+                    {/*INYECTADO LINK AL BOTON*/}
+                    <a href="asignaturas.html">
+                  <input id="btn1" type="submit" defaultValue="Registrarse" />
+                </a>  
+            </div>
             </div>
           </div>
         </div></section>
--- FRONTEND/semillita/src/components/NosotrosComponent.js
+++ FRONTEND/semillita/src/components/NosotrosComponent.js
@@ -3,56 +3,69 @@ import MAR from "../imagenes/MAR.png"
 import MIR from "../imagenes/MIR.png"
 import PR from "../imagenes/PR.png"
 import AV from "../imagenes/AV.png"
+import Mati from "../imagenes/mati.png"
+import Ali from "../imagenes/ali.png"
+import Paulino from "../imagenes/paulino.png"
+import Micha from "../imagenes/micha.png"
 
 const NosotrosComponent = () =>
 
 <div>
-  <h3 className="subtitulo">¿Quienes somos?</h3>
-  
-  <section className="testi">
-    <div className="MDP">
-      <div className="MiR">
-        <h2 className="subNombre">ID_MiR</h2>
-        <img className="ID" src={MIR} alt="MIR" />
-        <p>
-          “Me inspira el proyecto porque en el futuro <br /> me gustaría que en
-          Chile se enseñara programación <br />
-          como parte curriculum en todos los colegios.”
-          <br />
-          <br />
-          <span className="name">-Michael Roldan.</span>
-        </p>
-      </div>
-      <div className="PR">
-        <h2 className="subNombre">ID_PR</h2>
-        <img className="ID" src={PR} alt="PR" />
-        <p>
-          “Me inspira el proyecto porque le damos la oportunidad
-          <br /> a los niños de aprender ... de manera didáctica.”
-          <br />
-          <br />
-          <span className="name">-Paulino Rubio.</span>
-        </p>
-      </div>
-    </div>
-    <div className="AM">
-      <div className="AV">
-        <h2 className="subNombre">ID_AV</h2>
-        <img className="ID" src={AV} alt="AV" />
-        <p>
-          "Si mediante la pagina que estamos creando puedo enseñar
-          <br /> a pequeñas Alondritas a programar no lo voy a pensar
-          <br /> dos veces y voy a dar todo lo que pueda para hacerlo realidad "
-          <br />
-          <br />
-          <span className="name">-Alondra Valenzuela.</span>
-        </p>
-      </div>
-      <div className="MaR">
-        <h2 className="subNombre">ID_MaR</h2>
+    <h3 className="subtitulo">¿Quienes somos?</h3>
+
+    <section className="testi">
+        <div className="MDP">
+            <div className="MiR">
+                <h2 className="subNombre">ID_MiR</h2>
+                <div className="contenedor">
+                    <img src={MIR} alt="MiR" />
+                    <img className="top" src={Micha} alt="MiR" />
+                </div>
+                <p>
+                    “Me inspira el proyecto porque en el futuro <br /> me gustaría que en
+                    Chile se enseñara programación <br />
+                    como parte curriculum en todos los colegios.”
+                    <br />
+                    <br />
+                    <span className="name">-Michael Roldan.</span>
+                </p>
+            </div>
+            <div className="PR">
+                <h2 className="subNombre">ID_PR</h2>
+                <div className="contenedor">
+                    <img src={PR} alt="PR" />
+                    <img className="top" src={Paulino} alt="PR" />
+                </div>
+                <p>
+                    “Me inspira el proyecto porque le damos la oportunidad
+                    <br /> a los niños de aprender ... de manera didáctica.”
+                    <br />
+                    <br />
+                    <span className="name">-Paulino Rubio.</span>
+                </p>
+            </div>
+        </div>
+        <div className="AM">
+            <div className="AV">
+                <h2 className="subNombre">ID_AV</h2>
+                <div className="contenedor">
+                    <img src={AV} alt="AV" />
+                    <img className="top" src={Ali} alt="AV" />
+                </div>
+                <p>
+                    "Si mediante la pagina que estamos creando puedo enseñar
+                    <br /> a pequeñas Alondritas a programar no lo voy a pensar
+                    <br /> dos veces y voy a dar todo lo que pueda para hacerlo realidad "
+                    <br />
+                    <br />
+                    <span className="name">-Alondra Valenzuela.</span>
+                </p>
+            </div>
+            <div className="MaR">
+                <h2 className="subNombre">ID_MaR</h2>
                 <div className="contenedor">
                     <img src={MAR} alt="MAR" />
-                    <img className="top" src={AV} alt="MAR" />
+                    <img className="top" src={Mati} alt="MAR" />
                 </div>
             <p>
                 “La posibilidad de unir elementos del pasado <br /> con elementos del
@@ -64,7 +77,7 @@ const NosotrosComponent = () =>
             </p>
             </div>
         </div>    
-  </section>
+    </section>
 </div>
 
 export default NosotrosComponent;
